e545cf3c46fefa65a7cfa302c73003cd93e5b47d
Tobias Mucke <tobias.mucke@gmail.com>
2020-02-04 00:00:12 +0100

    Add manipulation of DOM with SVG elements. In addition some simple tests with CSS.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -46,35 +46,30 @@ pub fn append(selector: &str, element: &str) -> Result<web_sys::Element, JsValue
   let document = window.document().expect("should have a document on window");
   let body = document.body().expect("document expect to have have a body");
   
-  let svg = document.create_element("svg").unwrap();
+  let svg = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "svg")?;
   svg.set_attribute("width", "300");
   svg.set_attribute("height", "300");
   
-  let rect = document.create_element("rect")?;
-  rect.set_attribute("x", "10.0");
-  rect.set_attribute("y", "10.0");
-  rect.set_attribute("width", "150.0");
-  rect.set_attribute("height", "150.0");
-  rect.set_attribute("fill", "blue")?;
+  let rect1 = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "rect")?;
+  rect1.set_attribute("x", "1.0");
+  rect1.set_attribute("y", "1.0");
+  rect1.set_attribute("width", "150.0");
+  rect1.set_attribute("height", "150.0");
+  rect1.set_attribute("class", "rect");
+  rect1.set_id("rect1");
   
-  svg.append_child(&rect)?;
+  let rect2 = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "rect")?;
+  rect2.set_attribute("x", "21.0");
+  rect2.set_attribute("y", "21.0");
+  rect2.set_attribute("width", "50.0");
+  rect2.set_attribute("height", "50.0");
+  rect2.set_attribute("class", "rect");
+  rect2.set_id("rect2");
+  
+  
+  svg.append_child(&rect1)?;
+  svg.append_child(&rect2)?;
   body.append_child(&svg)?;
   
-  Ok((rect))
-/*
-  let sel = document.query_selector(selector)?.unwrap();
-  let el = document.create_element(element)?;
-  
-  let e = sel.insert_adjacent_element("afterbegin", &el)?.unwrap();
-  e.set_attribute("width", "300");
-  e.set_attribute("height", "300");
-  
-  let rect = document.create_element("rect")?.dyn_into::<web_sys::SvgRect>()?;
-  rect.set_x(10.0);
-  rect.set_y(10.0);
-  rect.set_width(150.0);
-  rect.set_height(150.0);
-  
-  e.insert_adjacent_element("afterbegin", &rect)?.unwrap();
-*/
+  Ok(rect2)
 }
